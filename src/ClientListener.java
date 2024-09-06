96db58996c690ba3fbd0dca0961839f40c89eb66
Prisha Singh <160752420+programmer1000001@users.noreply.github.com>
2024-09-05 20:34:19 -0500

    Update ClientListener.java

--- src/ClientListener.java
+++ src/ClientListener.java
@@ -1,2 +1,33 @@
 public class ClientListener {
+  private Socket socket; 
+  private ObjectInputStream inputStream; 
+  private ObjectOutputStream outputStream;
+  private Game game; 
+
+  public ClientListener(ObjectInputStream is, ObjectOutputStream os, Game game){
+    this.inputStream = is; 
+    this.outputStream = os; 
+    this.game = game; 
+  }
+  public void run() {
+        try {
+            while (true) {
+                // Read message from the server
+                CommandFromServer cfs = (CommandFromServer) inputStream.readObject();
+
+                // Process the received message
+                if(cfs.getCommand() == CommandFromServer.PLAY) {
+                   
+                }
+                if(cfs.getCommand() == CommandFromServer.ADD_HERO){
+                    
+                }
+                if(cfs.getCommand() == CommandFromServer.DISCONNECT){
+                    
+                }
+            }
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 }
