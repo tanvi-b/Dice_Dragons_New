e2642e71ac03a990d43dcc5080c664465faac19c
tanvi-b <bhattadtanvi@gmail.com>
2024-09-10 17:02:03 -0500

    -loaded all images onto game

--- src/Game.java
+++ src/Game.java
@@ -32,31 +32,16 @@ public class Game extends JFrame {
     private ArrayList<BufferedImage> dragonGuide;
     private ArrayList<BufferedImage> dragonSheets;
     private ArrayList<BufferedImage> heroSheets;
-    private BufferedImage intro, loginBackground, background, weapon;
+    private BufferedImage intro, loginBackground, background;
+    //playing tokens
+    //circular tokens
 
     public Game()
     {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setTitle("Dice and Dragons Board Game");
         diceFaces = new ArrayList<>();
-
-        try
-        {
-            intro = ImageIO.read(new File("images/introScreen.jpg"));
-            loginBackground = ImageIO.read(new File("images/loginScreen.jpg"));
-            background = ImageIO.read(new File("images/backgroundImage.png"));
-            diceFaces.add(ImageIO.read(new File("images/dice side.png")));
-            diceFaces.add(ImageIO.read(new File("images/D&D dice_001.png")));
-            diceFaces.add(ImageIO.read(new File("images/D&D dice_002.png")));
-            diceFaces.add(ImageIO.read(new File("images/D&D dice_004.png")));
-            diceFaces.add(ImageIO.read(new File("images/D&D dice_005.png")));
-            diceFaces.add(ImageIO.read(new File("images/D&D dice_006.png")));
-        }
-        catch(Exception e)
-        {
-            e.printStackTrace();
-        }
-
+        readImages();
         JPanel introScreen = new JPanel() {
             @Override
             protected void paintComponent(Graphics g) {
@@ -718,5 +703,70 @@ public class Game extends JFrame {
         button.setContentAreaFilled(false);
         button.setFocusPainted(false);
         button.setVisible(true);
+    }
+
+    public void readImages()
+    {
+        try
+        {
+            intro = ImageIO.read(new File("images/introScreen.jpg"));
+            loginBackground = ImageIO.read(new File("images/loginScreen.jpg"));
+            background = ImageIO.read(new File("images/backgroundImage.png"));
+            diceFaces.add(ImageIO.read(new File("images/dice side.png")));
+            diceFaces.add(ImageIO.read(new File("images/D&D dice_001.png")));
+            diceFaces.add(ImageIO.read(new File("images/D&D dice_002.png")));
+            diceFaces.add(ImageIO.read(new File("images/D&D dice_004.png")));
+            diceFaces.add(ImageIO.read(new File("images/D&D dice_005.png")));
+            diceFaces.add(ImageIO.read(new File("images/D&D dice_006.png")));
+            playerRules.add(ImageIO.read(new File("rules/playerRules/1.png")));
+            playerRules.add(ImageIO.read(new File("rules/playerRules/2.png")));
+            playerRules.add(ImageIO.read(new File("rules/playerRules/3.png")));
+            playerRules.add(ImageIO.read(new File("rules/playerRules/4.png")));
+            playerRules.add(ImageIO.read(new File("rules/playerRules/5.png")));
+            playerRules.add(ImageIO.read(new File("rules/playerRules/6.png")));
+            playerRules.add(ImageIO.read(new File("rules/playerRules/7.png")));
+            playerRules.add(ImageIO.read(new File("rules/playerRules/8.png")));
+            playerRules.add(ImageIO.read(new File("rules/playerRules/9.png")));
+            playerRules.add(ImageIO.read(new File("rules/playerRules/page10.png")));
+            playerRules.add(ImageIO.read(new File("rules/playerRules/page11.png")));
+            playerRules.add(ImageIO.read(new File("rules/playerRules/page12.png")));
+            dragonGuide.add(ImageIO.read(new File("rules/dragonRules/D&D dragons guide-01.png")));
+            dragonGuide.add(ImageIO.read(new File("rules/dragonRules/D&D dragons guide-02.png")));
+            dragonGuide.add(ImageIO.read(new File("rules/dragonRules/D&D dragons guide-03.png")));
+            dragonGuide.add(ImageIO.read(new File("rules/dragonRules/D&D dragons guide-04.png")));
+            dragonGuide.add(ImageIO.read(new File("rules/dragonRules/D&D dragons guide-05.png")));
+            dragonGuide.add(ImageIO.read(new File("rules/dragonRules/D&D dragons guide-07.png")));
+            dragonGuide.add(ImageIO.read(new File("rules/dragonRules/D&D dragons guide-08.png")));
+            dragonGuide.add(ImageIO.read(new File("rules/dragonRules/D&D dragons guide-09.png")));
+            dragonGuide.add(ImageIO.read(new File("rules/dragonRules/D&D dragons guide-10.png")));
+            dragonGuide.add(ImageIO.read(new File("rules/dragonRules/D&D dragons guide-11.png")));
+            dragonGuide.add(ImageIO.read(new File("rules/dragonRules/D&D dragons guide-12.png")));
+            dragonGuide.add(ImageIO.read(new File("rules/dragonRules/D&D dragons guide-13.png")));
+            dragonGuide.add(ImageIO.read(new File("rules/dragonRules/D&D dragons guide-14.png")));
+            dragonGuide.add(ImageIO.read(new File("rules/dragonRules/D&D dragons guide-15.png")));
+            dragonGuide.add(ImageIO.read(new File("rules/dragonRules/D&D dragons guide-16.png")));
+            dragonGuide.add(ImageIO.read(new File("rules/dragonRules/D&D dragons guide-17.png")));
+            dragonGuide.add(ImageIO.read(new File("rules/dragonRules/D&D dragons guide-18.png")));
+            dragonGuide.add(ImageIO.read(new File("rules/dragonRules/D&D dragons guide-19.png")));
+            dragonGuide.add(ImageIO.read(new File("rules/dragonRules/D&D dragons guide-20.png")));
+            dragonSheets.add(ImageIO.read(new File("images/black dragon.png")));
+            dragonSheets.add(ImageIO.read(new File("images/blue dragon.png")));
+            dragonSheets.add(ImageIO.read(new File("images/green dragon.png")));
+            dragonSheets.add(ImageIO.read(new File("images/red dragon.png")));
+            dragonSheets.add(ImageIO.read(new File("images/undead dragon.png")));
+            dragonSheets.add(ImageIO.read(new File("images/young black dragon.png")));
+            dragonSheets.add(ImageIO.read(new File("images/young red dragon.png")));
+//add pale dragon
+            heroSheets.add(ImageIO.read(new File("images/cleric.png")));
+            heroSheets.add(ImageIO.read(new File("images/custom hero.png")));
+            heroSheets.add(ImageIO.read(new File("images/ranger.png")));
+            heroSheets.add(ImageIO.read(new File("images/rogue.png")));
+            heroSheets.add(ImageIO.read(new File("images/warrior.png")));
+            heroSheets.add(ImageIO.read(new File("images/wizard.png")));
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
     }
 }
