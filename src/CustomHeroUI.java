14ebf66950b0ef6480693577bbaf0fcfe1f17c9c
tanvi-b <bhattadtanvi@gmail.com>
2024-09-13 17:42:07 -0500

    -created separate classes for each screen and used card layout to smoothly transition between each

--- src/CustomHeroUI.java
+++ src/CustomHeroUI.java
@@ -1,2 +1,228 @@
-public class CustomHeroUI {
-}
+//import javax.imageio.ImageIO;
+//import javax.swing.*;
+//import javax.swing.event.ListSelectionEvent;
+//import javax.swing.event.ListSelectionListener;
+//import javax.swing.table.DefaultTableModel;
+//import java.awt.*;
+//import java.awt.event.ActionEvent;
+//import java.awt.event.ActionListener;
+//import java.awt.image.BufferedImage;
+//import java.io.File;
+//import java.io.IOException;
+//import java.util.ArrayList;
+//
+////take the previous screen in constructor?
+//public class CustomHeroUI extends JPanel{
+//    boolean rollClicked;
+//    boolean host;//figure out to get from host ui later
+//    private ArrayList<BufferedImage> diceFaces;
+//    private BufferedImage loginBackground;
+//    private Font customFont;
+//    private Font customBoldFont;
+//
+//    public CustomHeroUI()
+//    {
+//        rollClicked = false;
+//        host = false; //figure out how to get from into later
+//        diceFaces = new ArrayList<>();
+//        setLayout(null);
+//        readImages();
+//        loadFonts();
+//        addComponents();
+//    }
+//
+//    protected void paintComponent(Graphics g) {
+//        super.paintComponent(g);
+//        g.drawImage(loginBackground, 0, 0, 1200, 1000, this);
+//        if (rollClicked==false) {
+//            g.drawImage(diceFaces.get(0), 550, 120, 100, 100, this);
+//            g.drawImage(diceFaces.get(0), 675, 120, 100, 100, this);
+//            g.drawImage(diceFaces.get(0), 800, 120, 100, 100, this);
+//            g.drawImage(diceFaces.get(0), 925, 120, 100, 100, this);
+//            g.drawImage(diceFaces.get(0), 1050, 120, 100, 100, this);
+//        }
+//    }
+//
+//    private void addComponents()
+//    {
+//        DefaultTableModel skillsModel = new DefaultTableModel() {
+//            @Override
+//            public boolean isCellEditable(int row, int column) {
+//                return false;
+//            }
+//        };
+//        skillsModel.addColumn("Skill");
+//        skillsModel.addColumn("Class(es)");
+//        skillsModel.addColumn("Effect");
+//        JTable skillsTable = new JTable(skillsModel);
+//        skillsTable.getTableHeader().setFont(new Font("Times New Roman", Font.BOLD, 18));
+//        skillsTable.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+//        JScrollPane skillsScrollPane = new JScrollPane(skillsTable);
+//        skillsScrollPane.setBounds(20, 35, 500, 680);
+//
+//        ListSelectionModel selectionModel = skillsTable.getSelectionModel();
+//        selectionModel.addListSelectionListener(new ListSelectionListener() {
+//            @Override
+//            public void valueChanged(ListSelectionEvent e) {
+//                if (skillsTable.getSelectedRowCount() > 6) {
+//                    //prevent users from selecting more
+//                }
+//            }
+//        });
+//        skillsTable.setBounds(20, 35, 500, 680);
+//        skillsTable.setOpaque(true);
+//
+//        JLabel diceMessage = new JLabel("Roll the dice 3 times to determine your hit points and armor class.");
+//        diceMessage.setForeground(Color.white);
+//        diceMessage.setFont(customFont.deriveFont(25f));
+//        diceMessage.setBounds(530, 30, 700, 100);
+//
+//        JLabel rollingDice = new JLabel("Rolling dice...");
+//        rollingDice.setHorizontalAlignment(SwingConstants.CENTER);
+//        rollingDice.setForeground(Color.red);
+//        rollingDice.setFont(customFont.deriveFont(45f));
+//        rollingDice.setBounds(600, 120, 500, 75);
+//        rollingDice.setOpaque(true);
+//
+//        JButton roll = new JButton ("Roll");
+//        roll.setForeground(Color.white);
+//        roll.setFont(customFont.deriveFont(45f));
+//        roll.setBounds(630, 250, 425, 50);
+//        roll.setBorder(BorderFactory.createLineBorder(Color.white));
+//        buttonFormatting(roll);
+//        roll.addActionListener(new ActionListener() {
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//                rollClicked = true;
+//                rollingDice.setVisible(true);
+//                validate();
+//                repaint();
+//                setVisible(true);
+//            }
+//        });
+//
+//        JLabel hitPoints = new JLabel("Hit Points:");
+//        hitPoints.setForeground(Color.white);
+//        hitPoints.setFont(customFont.deriveFont(45f));
+//        hitPoints.setBounds(550, 375, 700, 100);
+//
+//        JTextField hitPointsText = new JTextField();
+//        hitPointsText.setFont(new Font("Arial", Font.PLAIN, 20));
+//        hitPointsText.setBounds(800, 375, 350, 75);
+//        hitPointsText.setEditable(false);
+//        hitPointsText.setText("calculate");
+//
+//        JLabel armorClass = new JLabel("Armor Class:");
+//        armorClass.setForeground(Color.white);
+//        armorClass.setFont(customFont.deriveFont(45f));
+//        armorClass.setBounds(550, 510, 700, 100);
+//
+//        JTextField armorClassText = new JTextField();
+//        armorClassText.setFont(new Font("Arial", Font.PLAIN, 20));
+//        armorClassText.setBounds(800, 510, 350, 75);
+//        armorClassText.setEditable(false);
+//        armorClassText.setText("calculate");
+//
+//        JButton back2 = new JButton ("Back");
+//        back2.setForeground(new Color(204, 185, 45));
+//        back2.setFont(customFont.deriveFont(30f));
+//        back2.setBounds(5, 730, 130, 50);
+//        back2.setBorder(BorderFactory.createLineBorder(Color.white));
+//        buttonFormatting(back2);
+//        back2.addActionListener(new ActionListener() {
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//                removeAll();
+//                if (host) {
+//                    add(new HostUI()); //dont make new, somehow access previous one
+//                    rollClicked=false;
+//                    rollingDice.setVisible(false);
+//                }
+//                else {
+//                    add(new JoinUI()); //dont make new, somehow access previous one
+//                    rollClicked=false;
+//                    rollingDice.setVisible(false);
+//                }
+//                validate();
+//                repaint();
+//                setVisible(true);
+//            }
+//        });
+//
+//        JButton createHero = new JButton("Create Hero");
+//        createHero.setForeground(Color.white);
+//        createHero.setFont(customFont.deriveFont(75f));
+//        createHero.setBounds(600, 620, 500, 100);
+//        createHero.setBorderPainted(false);
+//        buttonFormatting(createHero);
+//        createHero.addActionListener(new ActionListener() {
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//                removeAll();
+//                if (host) {
+////                    getContentPane().add(hostScreen);
+////                    heroClassChoice1.setSelectedIndex(0);
+////                    heroClassChoice1.setVisible(false);
+////                    custom1.setVisible(false);
+////                    customHeroMade1.setVisible(true);
+//                }
+//                else {
+////                    getContentPane().add(joinScreen);
+////                    heroClassChoice.setSelectedIndex(0);
+////                    heroClassChoice.setVisible(false);
+////                    custom.setVisible(false);
+////                    customHeroMade.setVisible(true);
+//                }
+//                validate();
+//                repaint();
+//                setVisible(true);
+//            }
+//        });
+//
+//        add(skillsScrollPane);
+//        add(diceMessage);
+//        add(roll);
+//        add(hitPoints);
+//        add(hitPointsText);
+//        add(armorClass);
+//        add(armorClassText);
+//        add(back2);
+//        add(createHero);
+//        add(rollingDice);
+//        rollingDice.setVisible(false);
+//    }
+//
+//    private void buttonFormatting (JButton button)
+//    {
+//        button.setOpaque(false);
+//        button.setContentAreaFilled(false);
+//        button.setFocusPainted(false);
+//        button.setVisible(true);
+//    }
+//    private void readImages()
+//    {
+//        try
+//        {
+//            loginBackground = ImageIO.read(new File("images/loginScreen.jpg"));
+//            diceFaces.add(ImageIO.read(new File("images/dice side.png")));
+//            diceFaces.add(ImageIO.read(new File("images/D&D dice_001.png")));
+//            diceFaces.add(ImageIO.read(new File("images/D&D dice_002.png")));
+//            diceFaces.add(ImageIO.read(new File("images/D&D dice_004.png")));
+//            diceFaces.add(ImageIO.read(new File("images/D&D dice_005.png")));
+//            diceFaces.add(ImageIO.read(new File("images/D&D dice_006.png")));
+//        }
+//        catch(Exception e)
+//        {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    private void loadFonts() {
+//        try {
+//            customFont = Font.createFont(Font.TRUETYPE_FONT, new File("fonts/Almendra-Regular.ttf"));
+//            customBoldFont = Font.createFont(Font.TRUETYPE_FONT, new File("fonts/Almendra-Bold.ttf"));
+//        } catch (IOException | FontFormatException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//}
---
+++ src/JoinUI.java
@@ -0,0 +1,212 @@
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class JoinUI extends JPanel {
+    Game game;
+    private CardLayout cardLayout;
+    private JPanel mainPanel;
+    private BufferedImage loginBackground;
+    private Font customFont;
+    private Font customBoldFont;
+
+    public JoinUI(CardLayout cardLayout, JPanel mainPanel) {
+        this.game = game; //access game object from game ui
+        this.cardLayout = cardLayout;
+        this.mainPanel = mainPanel;
+        setLayout(null);
+        readImages();
+        loadFonts();
+        addComponents();
+    }
+
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(loginBackground, 0, 0, 1200, 1000, this);
+    }
+
+    private void addComponents() {
+        JLabel message = new JLabel("Ask the host for the access code. It is on the top-right of their screen.");
+        message.setForeground(Color.white);
+        message.setFont(customFont.deriveFont(40f));
+        message.setBounds(50, 30, 1300, 100);
+
+        JLabel accessCode = new JLabel("Enter access code:");
+        accessCode.setForeground(Color.black);
+        accessCode.setFont(customFont.deriveFont(55f));
+        accessCode.setBounds(80, 200, 500, 100);
+        accessCode.setHorizontalAlignment(SwingConstants.RIGHT);
+
+        JLabel characterName = new JLabel("Enter character name:");
+        characterName.setForeground(Color.black);
+        characterName.setFont(customFont.deriveFont(55f));
+        characterName.setBounds(80, 350, 500, 100);
+        characterName.setHorizontalAlignment(SwingConstants.RIGHT);
+
+        JLabel heroClass = new JLabel("Choose your hero class:");
+        heroClass.setForeground(Color.black);
+        heroClass.setFont(customFont.deriveFont(50f));
+        heroClass.setBounds(80, 500, 500, 100);
+        heroClass.setHorizontalAlignment(SwingConstants.RIGHT);
+
+        JTextField accessCodeText = new JTextField();
+        accessCodeText.setFont(new Font("Arial", Font.PLAIN, 20));
+        accessCodeText.setBounds(600, 200, 350, 75);
+        accessCodeText.setEditable(true);
+
+        JLabel invalidAccessCode = new JLabel("Invalid access code");
+        invalidAccessCode.setHorizontalAlignment(SwingConstants.CENTER);
+        invalidAccessCode.setForeground(Color.red);
+        invalidAccessCode.setFont(customFont.deriveFont(20f));
+        invalidAccessCode.setBounds(950, 213, 245, 50);
+        invalidAccessCode.setOpaque(true);
+
+        JTextField characterNameText = new JTextField();
+        characterNameText.setFont(new Font("Arial", Font.PLAIN, 20));
+        characterNameText.setBounds(600, 350, 350, 75);
+        characterNameText.setEditable(true);
+
+        JLabel duplicateName = new JLabel("Name has already been taken");
+        duplicateName.setHorizontalAlignment(SwingConstants.CENTER);
+        duplicateName.setForeground(Color.red);
+        duplicateName.setFont(customFont.deriveFont(20f));
+        duplicateName.setBounds(950, 360, 245, 50);
+        duplicateName.setOpaque(true);
+
+        ArrayList<String> availableHeroClasses = new ArrayList<>();
+        availableHeroClasses.add("Warrior");
+        availableHeroClasses.add("Wizard");
+        availableHeroClasses.add("Cleric");
+        availableHeroClasses.add("Ranger");
+        availableHeroClasses.add("Rogue");
+
+//        for (int i = 4; i>=0; i--)
+//        {
+//            for (int j = 0; j<game.heroes.size(); j++)
+//            {
+//                if (game.heroes.get(j).classType==i)
+//                    availableHeroClasses.remove(i);
+//            }
+//        }
+
+        JComboBox<String> heroClassChoice = new JComboBox<>(availableHeroClasses.toArray(new String[0]));
+        heroClassChoice.setSize(350, 75);
+        heroClassChoice.setLocation(600, 500);
+
+        JButton custom = new JButton("Custom");
+        custom.setForeground(new Color(204, 185, 45));
+        custom.setFont(customFont.deriveFont(35f));
+        custom.setBounds(1000, 525, 155, 50);
+        custom.setBorder(BorderFactory.createLineBorder(Color.white));
+        buttonFormatting(custom);
+        custom.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                removeAll();
+                //add(new CustomHeroUI());
+                validate();
+                repaint();
+                setVisible(true);
+            }
+        });
+
+        JButton beginGame = new JButton ("Begin Game");
+        beginGame.setForeground(Color.white);
+        beginGame.setFont(customFont.deriveFont(75f));
+        beginGame.setBounds(400, 650, 500, 100);
+        beginGame.setBorderPainted(false);
+        buttonFormatting(beginGame);
+        beginGame.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //send message to server and validate input
+                //if access code is not valid display jlabel
+                //if name is duplicate display jlabel
+                if (!accessCodeText.getText().equals("") && !characterNameText.getText().equals("") &&
+                        heroClassChoice.getSelectedIndex()!=-1) {
+                    removeAll();
+                    //add(new LobbyUI());
+                    validate();
+                    repaint();
+                    setVisible(true);
+                }
+            }
+        });
+
+        //jlabel saying max players reached or once game starts, make the access code invalid?
+
+        JLabel customHeroMade = new JLabel("Custom hero has been made");
+        customHeroMade.setForeground(Color.red);
+        customHeroMade.setFont(customFont.deriveFont(38f));
+        customHeroMade.setBounds(600, 500, 700, 100);
+
+        JButton back = new JButton("Back");
+        back.setForeground(new Color(204, 185, 45));
+        back.setFont(customFont.deriveFont(30f));
+        back.setBounds(5, 730, 130, 50);
+        back.setBorder(BorderFactory.createLineBorder(Color.white));
+        buttonFormatting(back);
+        back.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                heroClassChoice.setSelectedIndex(-1);
+                accessCodeText.setText("");
+                characterNameText.setText("");
+                heroClassChoice.setVisible(true);
+                custom.setVisible(true);
+                customHeroMade.setVisible(false);
+                cardLayout.show(mainPanel, "IntroScreen");
+            }
+        });
+
+        add(message);
+        add(accessCode);
+        add(accessCodeText);
+        add(characterName);
+        add(characterNameText);
+        add(heroClass);
+        add(heroClassChoice);
+        heroClassChoice.setSelectedIndex(-1);
+        add(custom);
+        add(beginGame);
+        add(back);
+        add(customHeroMade);
+        add(invalidAccessCode);
+        add(duplicateName);
+        customHeroMade.setVisible(false);
+        invalidAccessCode.setVisible(false);
+        duplicateName.setVisible(false);
+    }
+
+    private void buttonFormatting(JButton button) {
+        button.setOpaque(false);
+        button.setContentAreaFilled(false);
+        button.setFocusPainted(false);
+        button.setVisible(true);
+    }
+
+    private void readImages() {
+        try {
+            loginBackground = ImageIO.read(new File("images/loginScreen.jpg"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void loadFonts() {
+        try {
+            customFont = Font.createFont(Font.TRUETYPE_FONT, new File("fonts/Almendra-Regular.ttf"));
+            customBoldFont = Font.createFont(Font.TRUETYPE_FONT, new File("fonts/Almendra-Bold.ttf"));
+        } catch (IOException | FontFormatException e) {
+            e.printStackTrace();
+        }
+    }
+}
