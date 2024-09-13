0cb3e44014feda51ced7908a1c4eb98df3d158a4
Prisha Singh <160752420+programmer1000001@users.noreply.github.com>
2024-09-12 23:43:54 -0500

    working on lobby screen networking

--- src/CommandFromServer.java
+++ src/CommandFromServer.java
@@ -4,20 +4,22 @@ public class CommandFromServer implements Serializable {
     private int command;
     private Object data = null;
     private String player = "";
+    private String message = "";
 
     // Command list
     public static final int ACCESS_CODE = 0;
     public static final int CONNECT = 1;
 
+
     public static final int INVALID_ACCESS_CODE = 2;
     public static final int INVALID_NAME = 3;
 
-    //public static final int MAKE_HERO = 4;
-    //public static final int CHAT = 5;
-    //public static final int DISCONNECT = 3;
-    //public static final int ADD_HERO = 4;
+    public static final int MAKE_HERO = 4;
+    public static final int CHAT = 5;
+    public static final int DISCONNECT = 3;
 
-    public CommandFromServer(int command, Object data, String player) {
+
+    public CommandFromServer(int command, Object data, String player, String message) {
         this.command = command;
         this.data = data;
         this.player = player;
@@ -34,4 +36,8 @@ public class CommandFromServer implements Serializable {
     public String getPlayer() {
         return player;
     }
+    public String getMessage(){
+        return message;
+    }
 }
+
