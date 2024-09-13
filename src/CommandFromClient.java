49bb87bae47869c10f8c548c2d42ddd7673c7408
tanvi <bhattadtanvi@gmail.com>
2024-09-13 09:21:04 -0500

    -updated cfc

--- src/CommandFromClient.java
+++ src/CommandFromClient.java
@@ -11,14 +11,11 @@ public class CommandFromClient implements Serializable
     public static final int JOIN=0;
     public static final int HOST=1;
 
-    public static final int JOIN=0;
-    public static final int HOST=1;
-
-    public static final int CUSTOM_HERO = 2;
-    public static final int CHAT = 3;
-    public static final int PLAY=4;
-    public static final int DISCONNECT = 5;
-    public static final int CONNECT = 6;
+    //public static final int CUSTOM_HERO = 2;
+    //public static final int CHAT = 3;
+//    public static final int PLAY=4;
+//    public static final int DISCONNECT = 5;
+//    public static final int CONNECT = 6;
 
 
 
@@ -37,7 +34,8 @@ public class CommandFromClient implements Serializable
     public String getPlayer(){
         return player;
     }
-    public String getMessage(){
-        return message;
-    }
+
+//    public String getMessage(){
+//        return message;
+//    }
 }
