ff1b886d26e2d232594c6db077d76b0d0c9ba022
Prisha Singh <160752420+programmer1000001@users.noreply.github.com>
2024-10-19 10:11:52 -0500

    token networking done

---
+++ src/Coordinate.java
@@ -0,0 +1,39 @@
+public class Coordinate implements Serializable {
+    private int x;
+    private int y;
+    private int hero;
+
+    public Coordinate(){
+
+    }
+    public Coordinate(int xCoor, int yCoor, int heroC){
+        x = xCoor;
+        y = yCoor;
+        hero = heroC;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getHero() {
+        return hero;
+    }
+
+    public void setHero(int hero) {
+        this.hero = hero;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+}
